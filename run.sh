a966d0f5b2e9c4ce3a2b667a4e208e68b97b320f
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2021-09-18 21:42:15 -0400

    Fixing pipfile

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
+pipenv lock --clear
 pipenv install
 pipenv run python feature_extraction.py --max_df 0.5 --max_features 1000
 pipenv run python find_centroids.py --max_df 0.5 --max_features 1000 --k 60
