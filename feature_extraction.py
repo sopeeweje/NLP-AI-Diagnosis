45e488e8a257d72605d0d6a4c606a8040f5cd8bf
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2021-09-27 12:11:16 -0400

    Implement stemming and lemmatization
    Stemming - e.g. A stemming algorithm reduces the words “chocolates”, “chocolatey”, “choco” to the root word, “chocolate” and “retrieval”, “retrieved”, “retrieves” reduce to the stem “retrieve”.
    
    Lemmatization - e.g. The word "better" has "good" as its lemma. This link is missed by stemming, as it requires a dictionary look-up.

--- feature_extraction.py
+++ feature_extraction.py
@@ -12,6 +12,8 @@ from collections import Counter
 import statistics
 import pickle
 import numpy as np
+from nltk import word_tokenize          
+from nltk.stem import WordNetLemmatizer, PorterStemmer
 import argparse
 
 def mk_int(s):
@@ -92,6 +94,16 @@ def process_data(data_file, funding_file):
     print("Processed data N: {}".format(str(len(data))))
     return data, test_data
 
+class LemmaStemmerTokenizer:
+    """
+    Tokenizer that lemmatizes and stems words
+    """
+    def __init__(self):
+        self.wnl = WordNetLemmatizer()
+        self.ps = PorterStemmer()
+    def __call__(self, doc):
+        return [self.ps.stem(self.wnl.lemmatize(t)) for t in word_tokenize(doc)]
+
 def feature_extraction(data, num_features, max_df):
     """
 
@@ -106,9 +118,10 @@ def feature_extraction(data, num_features, max_df):
         year - one-hot encoded year
         text - tfidf for all text data
     """
+    vect = TfidfVectorizer(tokenizer=LemmaStemmerTokenizer())  
     input_text = [item["text"] for item in data]
     print("Vectorizing...")
-    vectorizer = TfidfVectorizer(stop_words='english', ngram_range=(1,2), max_df=max_df, max_features=num_features).fit(input_text) #, token_pattern=u'(?ui)\\b\\w*[a-z]+\\w*\\b'
+    vectorizer = vect(stop_words='english', ngram_range=(1,2), max_df=max_df, max_features=num_features).fit(input_text) #, token_pattern=u'(?ui)\\b\\w*[a-z]+\\w*\\b'
     processed_text = vectorizer.transform(input_text)
     
     with open("processed-data.pkl", 'wb') as handle:
