a44c6539babb30b51ae4bcf90005fd84db88d3cf
Sope Eweje <feyisope.eweje@pennmedicine.upenn.edu>
2021-12-29 15:19:23 -0500

    Download nltk omw-1.4

--- setup.py
+++ setup.py
@@ -7,9 +7,18 @@ print("Creating directories...")
 
 # Create directories
 os.makedirs("data", exist_ok=True)
+file = open("data/.gitkeep", "w")
+file.close()
+
 os.makedirs("results", exist_ok=True)
+file = open("results/.gitkeep", "w")
+file.close()
+
 os.makedirs("figures", exist_ok=True)
+file = open("figures/.gitkeep", "w")
+file.close()
 
 # Natural Language Toolkit (NLTK) downloads
 nltk.download('punkt')
 nltk.download('wordnet')
+nltk.download('omw-1.4')
